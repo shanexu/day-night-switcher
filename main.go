159fbdae8b5dda4307f039fb1fa2fae6e9ea32a5
Shane Xu <xusheng0711@gmail.com>
2023-10-22 19:03:37 +0800

    add action

--- main.go
+++ main.go
@@ -11,10 +11,25 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-func dayNightThemeSwitch(str string) {
-	cmd := exec.Command("/home/shane/bin/night-theme-switch.sh", str)
+const (
+	VariantLight = "light"
+	VariantDark  = "dark"
+)
+
+func execAction(action []string) {
+	cmd := exec.Command(action[0], action[1:]...)
+	slog.Info("before exec action", "cmd", cmd)
 	if err := cmd.Run(); err != nil {
 		slog.Error("execute script failed", "err", err)
+	}
+}
+
+func dayNightSwitch(variant string, dayAction, nightAction []string) {
+	switch variant {
+	case VariantLight:
+		execAction(dayAction)
+	case VariantDark:
+		execAction(nightAction)
 	}
 }
 
@@ -23,12 +38,12 @@ func dayNight(dayBeginDuration, nightBeginDuration time.Duration) (string, time.
 	dayBegin := n.BeginningOfDay().Add(dayBeginDuration)
 	nightBegin := n.BeginningOfDay().Add(nightBeginDuration)
 	if n.Before(dayBegin) {
-		return "dark", n.Sub(dayBegin) * (-1)
+		return VariantDark, n.Sub(dayBegin) * (-1)
 	}
 	if n.Before(nightBegin) {
-		return "light", n.Sub(nightBegin) * (-1)
+		return VariantLight, n.Sub(nightBegin) * (-1)
 	}
-	return "dark", n.Sub(dayBegin.Add(24*time.Hour)) * (-1)
+	return VariantDark, n.Sub(dayBegin.Add(24*time.Hour)) * (-1)
 }
 
 func main() {
@@ -43,6 +58,7 @@ func main() {
 			panic(err)
 		}
 	}
+
 	viper.SetDefault("main.day_begin", "06:00:00")
 	viper.SetDefault("main.night_begin", "18:00:00")
 	dayBeginStr := viper.GetString("main.day_begin")
@@ -56,11 +72,19 @@ func main() {
 		panic(err)
 	}
 	if !dayBegin.Before(nightBegin) {
-		panic("dayBegin must less then nightBegin")
+		panic("day_begin must less then night_begin")
 	}
-
 	dayBeginDuration := now.New(dayBegin).Sub(now.New(dayBegin).BeginningOfDay())
 	nightBeginDuration := now.New(nightBegin).Sub(now.New(nightBegin).BeginningOfDay())
+
+	dayAction := viper.GetStringSlice("main.day_action")
+	if len(dayAction) == 0 {
+		panic("day_action must not be empty")
+	}
+	nightAction := viper.GetStringSlice("main.night_action")
+	if len(nightAction) == 0 {
+		panic("night_action must not be empty")
+	}
 
 	conn, err := dbus.ConnectSystemBus()
 	if err != nil {
@@ -86,7 +110,7 @@ func main() {
 	setTimerAndSwitchDayNight := func() {
 		variant, duration := dayNight(dayBeginDuration, nightBeginDuration)
 		slog.Info("switch to", "variant", variant)
-		dayNightThemeSwitch(variant)
+		dayNightSwitch(variant, dayAction, nightAction)
 		slog.Info("sleep", "duration", duration)
 		timer = time.AfterFunc(duration, func() {
 			eventChan <- struct{}{}
